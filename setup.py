9cb6de970797e1d85c51bde08f9cf7d13d00a975
Ross Pawley <ross.pawley@narwhalstudios.com>
2023-05-15 02:48:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ if __name__ == '__main__':
         author='Some Guy',
         packages=find_packages(),
         
-        install_requires=["img2texture @ git+https://github.com/WASasquatch/img2texture.git#egg=img2texture", "Pillow", "torch", "torchdiffeq", "torchsde", "einops", "open-clip-torch", "transformers==4.26.1", "safetensors>=0.3.0", "pytorch_lightning", "aiohttp", "accelerate", "pyyaml", "pilgram", "pythonperlin", "matplotlib", "numpy<1.24;>=1.18", "scikit-learn", "scikit-image==0.20.0", "scipy", "opencv-python-headless[ffmpeg]", "timm>=0.4.12", "gitpython", "fairscale>=0.4.4", "face_recognition", "imageio", "tqdm", "joblib"]
+        install_requires=["img2texture @ git+https://github.com/WASasquatch/img2texture.git#egg=img2texture", "Pillow", "torch", "torchdiffeq", 
+                          "torchsde", "einops", "open-clip-torch", "transformers==4.26.1", "safetensors>=0.3.0", "pytorch_lightning", "aiohttp",
+                          "accelerate", "pyyaml", "pilgram", "pythonperlin", "matplotlib", "numpy < 1.24 , >=1.18", "scikit-learn", "scikit-image==0.20.0", 
+                          "scipy", "opencv-python-headless[ffmpeg]", "timm>=0.4.12", "gitpython", "fairscale>=0.4.4", "face_recognition", "imageio", "tqdm", "joblib"]
                         
     )
